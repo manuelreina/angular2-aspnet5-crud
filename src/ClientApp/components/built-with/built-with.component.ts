b3c1934c5060dcaf4dd9e9bc0f1ebe6194399d81
mra_git <manuel.reina@hotmail.com>
2016-07-01 17:06:40 +0200

    built-with

---
+++ src/ClientApp/components/built-with/built-with.component.ts
@@ -0,0 +1,24 @@
+﻿import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router-deprecated';
+
+
+@Component({
+    selector: 'built-with',
+    template: require('./built-with.component.html'),
+    directives: []
+})
+export class BuiltWithComponent implements OnInit {
+
+
+    constructor(
+        
+    ) {
+        
+    }
+
+    
+
+    ngOnInit() {
+    }
+    
+}
